904872a2781e3214b98c74232657a62418fa9e9d
sairam2381 <sairam2381@gmail.com>
2023-10-29 09:29:16 +0530

    The changes are done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import HeroImage from "../../Assets/Hero.svg"
-import { HeroContainer,HeroImageContainer } from './style-hero'
+import React from "react";
+import HeroImage from "../../Assets/6101073.jpg";
+import { HeroContainer, HeroImageContainer } from "./style-hero";
 
-const Hero = ({setModal}) => {
+const Hero = ({ setModal }) => {
   return (
     <HeroContainer className="content_area">
-    <h1>
-      Creativity starts with viewing the whole world differently
-    </h1>
-    <p>A service agency with customer satisfaction</p>
+      <h1>Creativity starts with viewing the whole world differently</h1>
+      <p>A service agency with customer satisfaction</p>
 
-    <HeroImageContainer>
-      <img src={HeroImage} width="100%" alt="heroImage" />
-      <button type="button" onClick={() => {
-        setModal(true) 
-      }}>Get Started</button>
-    </HeroImageContainer>
-  </HeroContainer>
-  )
-}
+      <HeroImageContainer>
+        <img src={HeroImage} alt="heroImage" className="image__style" />
+        <button
+          type="button"
+          onClick={() => {
+            setModal(true);
+          }}
+        >
+          Get Started
+        </button>
+      </HeroImageContainer>
+    </HeroContainer>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Hero/style-hero.js
+++ src/components/Hero/style-hero.js
@@ -4,6 +4,7 @@ export const HeroContainer = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   gap: 1.6rem;
   padding: 2rem 0;
   z-index: 5;
@@ -19,6 +20,9 @@ export const HeroContainer = styled.section`
 `;
 
 export const HeroImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   &,
   & > img {
     position: relative;
@@ -43,6 +47,19 @@ export const HeroImageContainer = styled.div`
   @media screen and (min-width: 769px) {
     & > button {
       font-size: 1.125rem;
+    }
+  }
+  .image__style {
+    max-width: 700px;
+    height: 600px;
+    margin-top: 50px;
+    @media (max-width: 963px) {
+      max-width: 500px;
+      height: 400px;
+    }
+    @media (max-width: 400px) {
+      max-width: 300px;
+      height: 350px;
     }
   }
 `;
